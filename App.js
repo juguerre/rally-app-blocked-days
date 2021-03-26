90de633fa603ff0fca6299d5402abe7df4414819
Andrés Guerrero <juguerre@gmail.com>
2021-03-26 21:36:24 +0100

    Fix block days count, only inside iteration

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-Ext.define('CustomApp', {
+Ext.define('BlockedDays', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function () {
         //Write app code here
         //API Docs: https://help.rallydev.com/apps/2.1/doc/
+
         this.iterationCombobox = this.add({
             xtype: 'rallyiterationcombobox',
-            context: this.getContext(),
+            context: this.getContext().getDataContext(),
             listeners: {
                 ready: this._onIterationComboboxChanged,
                 select: this._onIterationComboboxChanged,
@@ -14,19 +15,24 @@ Ext.define('CustomApp', {
             }
         });
     },
-    _onIterationComboboxChanged: function () {        
+    _onIterationComboboxChanged: function () {
         let that = this;
+        if (this.grid) {
+            this.grid.setLoading(true);
+        }
+
         this.getSnapshotStore().load().then({
             success: function (records) {
-                let customRecords = that.getCustomStoreRecords(records);                
+                let customRecords = that.getCustomStoreRecords(records);
                 let customStore = Ext.create('Rally.data.custom.Store', {
                     data: customRecords
-                });                                
+                });
                 var mygrid = this.down('rallygrid');
                 that.remove(mygrid);
 
                 var gridConfig = this.getGrid(customStore);
-                that.grid = that.add(gridConfig);                
+                that.grid = that.add(gridConfig);
+                that.grid.setLoading(false);
             },
             scope: this
         }
@@ -40,6 +46,7 @@ Ext.define('CustomApp', {
             fetch: ['FormattedID', 'Name', 'ScheduleState', 'Iteration'],
             //autoLoad: true,
             compress: true,
+            //context: this.getContext().getDataContext(),
             context: {
                 workspace: this.getContext().getWorkspace(),
                 project: this.getContext().getProject(),
@@ -48,15 +55,23 @@ Ext.define('CustomApp', {
             },
             filters: [
                 {
+                    property: '_TypeHierarchy',
+                    value: 'HierarchicalRequirement'
+                },
+                {
                     property: 'Blocked',
-                    operator: '=',
                     value: true
                 },
                 {
                     property: 'Iteration',
-                    operator: '=',
                     value: iterid
                 },
+                //{
+                //    property: '_ValidFrom',
+                //    operator: '>',
+                //    value:
+//
+                //}
             ],
             sorters: [
                 {
@@ -79,16 +94,42 @@ Ext.define('CustomApp', {
 
         return snapshotStore;
     },
-    date_diff_indays: function (date1, date2) {
+    date_diff_indays: function (date1, date2, iterStartDate, iterEndDate) {
         let dt1 = new Date(date1);
         let dt2 = new Date(date2);
+        let iterStart = new Date(iterStartDate);
+        let iterEnd = new Date(iterEndDate);
+
+        let outOfBounds = false;
         if (dt1.getFullYear() === 9999) {
             dt1 = new Date();
         }
         if (dt2.getFullYear() === 9999) {
             dt2 = new Date();
         }
-        //return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), 
+        if (dt1.getTime() < iterStart.getTime()) {
+            dt1 = iterStart;
+            outOfBounds = true;
+        }
+        if (dt2.getTime() < iterStart.getTime()) {
+            dt2 = iterStart;
+            outOfBounds = true;
+        }
+        if (dt1.getTime() > iterEnd.getTime()) {
+            dt1 = iterEnd;
+            outOfBounds = true;
+        }
+
+        if (dt2.getTime() > iterEnd.getTime()){
+            dt2 = iterEnd;
+            outOfBounds = true;
+        }
+
+
+        if (outOfBounds){
+            console.log(date1, date2, iterStartDate, iterEndDate);
+        }
+        //return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(),
         //               dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
         return Math.floor((dt2.getTime() - dt1.getTime()) / (1000 * 60 * 60 * 24));
     },
@@ -105,22 +146,33 @@ Ext.define('CustomApp', {
                 { name: 'DaysBlocked', type: 'int', defaultValue: 0 }
             ],
             isUpdatable: function(){
-                console.log("No ...")
-                return false
+                console.log("No ...");
+                return false;
             }
         });
 
         for (var record of records) {
             // Init record for every formatedID
-            if (r_plus[record.get("FormattedID")] === undefined) {
-                r_plus[record.get("FormattedID")] = Ext.create('grouped_snapshots',{});                
-                Ext.apply(r_plus[record.get("FormattedID")].data, record.getData());
-                r_plus[record.get("FormattedID")].set("Iteration",record.get("Iteration").Name);
+            let formattedID = record.get("FormattedID")
+            if (r_plus[formattedID] === undefined) {
+                r_plus[formattedID] = Ext.create('grouped_snapshots',{});
+                Ext.apply(r_plus[formattedID].data, record.getData());
+                r_plus[formattedID].set("Iteration",record.get("Iteration").Name);
             }
 
-            r_plus[record.data.FormattedID].data.DaysBlocked = r_plus[record.data.FormattedID].data.DaysBlocked +
-                this.date_diff_indays(record.data._ValidFrom, record.data._ValidTo);
+            r_plus[formattedID].data.DaysBlocked = r_plus[formattedID].data.DaysBlocked +
+                this.date_diff_indays(record.data._ValidFrom,
+                    record.data._ValidTo,
+                    record.get("Iteration").StartDate,
+                    record.get("Iteration").EndDate);
         }
+
+        Object.keys(r_plus).forEach(fid => {
+            if (r_plus[fid].data.DaysBlocked === 0) {
+                delete r_plus[fid];
+            }
+        });
+
         return Object.values(r_plus);
     },
     getGrid: function (customStore){
@@ -129,8 +181,8 @@ Ext.define('CustomApp', {
             xtype: 'rallygrid',
             autoScroll: true,
             showPagingToolbar: false,
-            enableEditing: false, 
-            editable: false,           
+            enableEditing: false,
+            editable: false,
             columnCfgs: [
                 {
                     text: 'ID',
